8a930da152ba335d18841d11dcf415e2dc606321
Alfred Jophy <alfredjophy@protonmail.com>
2022-03-19 15:48:33 +0530

    added map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,28 +1,12 @@
 import { VectorMap } from "@react-jvectormap/core";
 import { worldMerc, worldMill } from "@react-jvectormap/world";
+import worldCustom from "../../assets/map.json";
 
 const Map = ({ width, height }) => {
-    const markers = [
-        {
-            latLng: [2.9, 12.45],
-            name: "Vatican City",
-            value: 200,
-        },
-        {
-            latLng: [11, 12.45],
-            name: "WOW",
-            value: 30,
-        },
-        {
-            latLng: [-33, 12.45],
-            name: "WAZZZA",
-            value: 16,
-        },
-    ];
     return (
         <div style={{ width: width, height: height }}>
             <VectorMap
-                map={worldMill}
+                map={worldCustom}
                 backgroundColor="#000"
                 markerStyle={{
                     initial: {
@@ -30,28 +14,10 @@ const Map = ({ width, height }) => {
                         stroke: "#383f47",
                     },
                 }}
-                series={{
-                    markers: [
-                        {
-                            attribute: "r",
-                            scale: [5, 20],
-                            values: [60, 6, 54],
-                            normalizeFunction: "polynomial",
-                        },
-                    ],
-                }}
                 regionStyle={{
                     initial: {
-                        fill: "#128da7",
+                        fill: "#FFF",
                     },
-                    hover: {
-                        fill: "#A0D1DC",
-                    },
-                }}
-                markers={markers}
-                style={{
-                    width: "100%",
-                    height: "100%",
                 }}
                 containerClassName="map"
             />
