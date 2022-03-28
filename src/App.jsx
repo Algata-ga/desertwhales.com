5faac937130a0803939aaefb88ab3264d11461e5
Alfred Jophy <alfredjophy@protonmail.com>
2022-03-28 21:28:55 +0530

    added custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,32 @@ import {
     Footer,
     KOIMap,
 } from "./sections";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
     return (
         <>
+            <AnimatedCursor
+                innerSize={15}
+                outerSize={19}
+                color="198,156,102"
+                outerAlpha={0.2}
+                innerScale={0.7}
+                outerScale={5}
+                clickables={[
+                    "a",
+                    'input[type="text"]',
+                    'input[type="email"]',
+                    'input[type="number"]',
+                    'input[type="submit"]',
+                    'input[type="image"]',
+                    "label[for]",
+                    "select",
+                    "textarea",
+                    "button",
+                    ".link",
+                ]}
+            />
             <Navbar />
             <Section1 />
             <Partner />
