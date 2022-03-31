b4b35a5fa35cb0cb19d3b3e951ddd856919f177e
Alfred Jophy <alfredjophy@protonmail.com>
2022-03-31 23:47:13 +0530

    map with pattern

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -16,7 +16,7 @@ const CustomMarker = ({ name, image, markerOffset }) => (
         <g transform="translate(0 -1028.4)">
             <path
                 d="m12 0c-4.4183 2.3685e-15 -8 3.5817-8 8 0 1.421 0.3816 2.75 1.0312 3.906 0.1079 0.192 0.221 0.381 0.3438 0.563l6.625 11.531 6.625-11.531c0.102-0.151 0.19-0.311 0.281-0.469l0.063-0.094c0.649-1.156 1.031-2.485 1.031-3.906 0-4.4183-3.582-8-8-8zm0 4c2.209 0 4 1.7909 4 4 0 2.209-1.791 4-4 4-2.2091 0-4-1.791-4-4 0-2.2091 1.7909-4 4-4z"
-                fill="#FFFddd"
+                fill="#FF3d2d"
                 transform="translate(0 1028.4)"
             />
             <path
@@ -44,8 +44,8 @@ const MapChart = ({ setTooltipContent, markers }) => {
                 {/* TODO: define custom pattern without clipping and we are good*/}
                 <PatternCircles
                     id={"mapCircleFill"}
-                    height={8}
-                    width={8}
+                    height={9}
+                    width={9}
                     fill="#FBDA83"
                     backgroundColor="black"
                     complement
@@ -60,17 +60,20 @@ const MapChart = ({ setTooltipContent, markers }) => {
                                     default: {
                                         fill: "url(#mapCircleFill)",
                                         outline: "none",
-                                        stroke: "none",
+                                        stroke: "#FBDA83",
+                                        strokeWidth: "0",
                                     },
                                     hover: {
                                         fill: "url(#mapCircleFill)",
                                         outline: "none",
-                                        stroke: "none",
+                                        stroke: "#FBDA83",
+                                        strokeWidth: "0",
                                     },
                                     pressed: {
                                         fill: "url(#mapCircleFill)",
                                         outline: "none",
-                                        stroke: "none",
+                                        stroke: "#FBDA83",
+                                        strokeWidth: "0",
                                     },
                                 }}
                             />
