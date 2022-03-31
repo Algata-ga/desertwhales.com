e26ab0a607e1d7867d028f4ae1c4381641d5352a
Alfred Jophy <alfredjophy@protonmail.com>
2022-03-31 23:46:49 +0530

    re-added particles

--- src/sections/section1/section1.jsx
+++ src/sections/section1/section1.jsx
@@ -25,16 +25,16 @@ const Section1 = () => {
                             You
                         </h1>
                         <div className={style.btns}>
-                        <a>
-                            <button className={style.contact}>
-                                Contact us
-                            </button>
-                        </a>
-                        <a href="#">
-                            <button className={style.academybtn}>
-                                Academy
-                            </button>
-                        </a>
+                            <a>
+                                <button className={style.contact}>
+                                    Contact us
+                                </button>
+                            </a>
+                            <a href="#">
+                                <button className={style.academybtn}>
+                                    Academy
+                                </button>
+                            </a>
                         </div>
                     </div>
                     <div className={style.bg}>
@@ -42,14 +42,14 @@ const Section1 = () => {
                     </div>
                 </Container>
             </section>
-            {/*<Particles
+            <Particles
                 init={particlesInit}
                 loaded={particlesLoaded}
                 width="100%"
                 height="calc(100vh - 100px)"
                 options={options}
                 style={{ top: "100px", zIndex: "-1", position: "absolute" }}
-    />*/}
+            />
         </>
     );
 };
