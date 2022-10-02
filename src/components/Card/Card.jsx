0aaf3e995d682973a149e0fd485918b28ce409ea
KKharidev <kkharidev@gmail.com>
2022-10-02 23:49:20 +0530

    Passed images to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,10 @@
 import style from "./Card.module.css";
 import icon from "../../assets/iconpng.png";
 import 'animate.css';
-const Card = ({title,body}) =>{
+const Card = ({image_url,title,body}) =>{
     return(
         <section className={style.card + " " + "wow animate__animated animate__fadeInLeft animate__repeat-1"}>
-            <img src={icon} alt="" />
+            <img src={image_url} alt="" />
             <h4>{title}</h4>
             <h6>{body}</h6>
        </section>
