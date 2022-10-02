517981b75315b0dc0f043b12696f70f79f8cb40f
KKharidev <kkharidev@gmail.com>
2022-10-02 23:49:53 +0530

    Added container

--- src/sections/Medium/Medium.jsx
+++ src/sections/Medium/Medium.jsx
@@ -8,6 +8,7 @@ import "swiper/css/pagination";
 import { Autoplay, EffectCoverflow, Pagination } from "swiper";
 
 import { useArticles } from "../../queries";
+import { Container } from "react-bootstrap";
 
 const Medium = () => {
     const articles = useArticles();
@@ -15,40 +16,42 @@ const Medium = () => {
     console.log(articles.data);
     return (
         <section className={style.medium}>
-            <Swiper
-                className={style.swiper + " " + "mySwiper"}
-                effect={"coverflow"}
-                grabCursor={false}
-                centeredSlides={true}
-                slidesPerView={"auto"}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                coverflowEffect={{
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 0,
-                    modifier: 1,
-                }}
-                modules={[Autoplay, EffectCoverflow, Pagination]}
-            >
-                {articles.data.map((item) => (
-                    <SwiperSlide className={style.slide}>
-                        <div className={style.imgbox}>
-                            <img src={item.banner} />
-                        </div>
-                        <h5>{item.title}</h5>
-                        <h6>
-                            {item.tags.reduce(
-                                (prev, cur) => prev + " " + cur,
-                                ""
-                            )}
-                        </h6>
-                        <p>{item.level}</p>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            <Container>
+                <Swiper
+                    className={style.swiper + " " + "mySwiper"}
+                    effect={"coverflow"}
+                    grabCursor={false}
+                    centeredSlides={true}
+                    slidesPerView={"auto"}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    coverflowEffect={{
+                        rotate: 0,
+                        stretch: 0,
+                        depth: 0,
+                        modifier: 1,
+                    }}
+                    modules={[Autoplay, EffectCoverflow, Pagination]}
+                >
+                    {articles.data.map((item) => (
+                        <SwiperSlide className={style.slide}>
+                            <div className={style.imgbox}>
+                                <img src={item.banner} />
+                            </div>
+                            <h5>{item.title}</h5>
+                            <h6>
+                                {item.tags.reduce(
+                                    (prev, cur) => prev + " " + cur,
+                                    ""
+                                )}
+                            </h6>
+                            <p>{item.level}</p>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </Container>
         </section>
     );
 };
