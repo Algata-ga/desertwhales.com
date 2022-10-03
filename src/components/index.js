9e48ad77a23160fa4c34846683d25a39d9d70df7
KKharidev <kkharidev@gmail.com>
2022-10-03 15:41:25 +0530

    Updated index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as MapChart } from "./map/map";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Card } from "./Card/Card";
 export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as ContactPopUp } from "./ContactPopUp/ContactPopUp";
